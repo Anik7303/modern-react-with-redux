e91cc8ae89b7108180f024a69c506d973a6f05c4
Anik7303 <anik7303@gmail.com>
2020-10-31 02:36:21 +0600

    songs project - finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,56 +1,21 @@
-import React, { useState } from "react";
-import Accordion from "./Accordion";
-import Search from "./Search";
-import Dropdown from "./Dropdown";
-import Translate from "./Translate";
-import Route from "./Route";
-import Header from "./Header";
+import React from "react";
 
-const items = [
-    { title: "What is React?", content: "React is a FrontEnd Javascript framework." },
-    { title: " Why use React?", content: "React is a favorite JS library among engineers." },
-    { title: "How do you use React?", content: "You use React by creating components." },
-];
+import SongList from "./SongList";
+import SongDetail from "./SongDetail";
 
-const options = [
-    {
-        label: "The Color Red",
-        value: "red",
-    },
-    {
-        label: "The Color Green",
-        value: "green",
-    },
-    {
-        label: "A Shade of Blue",
-        value: "blue",
-    },
-];
-
-function App(props) {
-    const [selected, setSelected] = useState(options[0]);
+const App = () => {
     return (
-        <div className="ui container">
-            <Header />
-            <Route path="/">
-                <Accordion items={items} />
-            </Route>
-            <Route path="/list">
-                <Search />
-            </Route>
-            <Route path="/dropdown">
-                <Dropdown
-                    selected={selected}
-                    onSelectedChanged={setSelected}
-                    options={options}
-                    label="Select a Color"
-                />
-            </Route>
-            <Route path="/translate">
-                <Translate />
-            </Route>
+        <div className="ui container grid">
+            <div className="ui row">
+                <div className="column eight wide">
+                    <SongList />
+                </div>
+                <div className="column eight wide">
+                    <SongDetail />
+                </div>
+            </div>
         </div>
     );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import "react-app-polyfill/ie9";
-import "react-app-polyfill/ie11";
-import "react-app-polyfill/stable";
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 import App from "./components/App";
+import reducers from "./reducers";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const store = createStore(reducers);
+
+const element = (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
